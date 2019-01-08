2f915f15d7a278d53ab31218180de7eef17885c6
Luca Delucchi <lucadeluge@gmail.com>
2019-01-08 16:19:32 +0100

    added setup.py

---
+++ setup.py
@@ -0,0 +1,24 @@
+# -*- coding: utf-8 -*-
+from setuptools import setup, find_packages
+
+# Get the long description from the relevant file
+with open('README.rst') as f:
+    long_description = f.read()
+
+setup(
+    name='caiosm',
+    version=__import__("caiosm").__version__,
+    description="Tools to work with Club Alpino Italiano OpenStreetMap data",
+    keywords="Club Alpino Italiano,CAI,OpenStreetMap,OSM",
+    packages=find_packages(),
+    include_package_data=False,
+    install_requires=open('requirements.txt').read().splitlines(),
+    long_description=long_description,
+    author='Luca Delucchi',
+    author_email='luca.delucchi@fmach.it',
+    license='GPLv3+',
+    entry_points="""
+    [console_scripts]
+    caisom=caiosm.scripts.caiosm:main
+    """
+)
