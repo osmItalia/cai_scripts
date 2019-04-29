af7a53446590a7b09ebb5d44d2e1676c1766468c
Luca Delucchi <lucadeluge@gmail.com>
2019-04-30 01:36:50 +0200

    removed old import

--- caiosm/__init__.py
+++ caiosm/__init__.py
@@ -1,7 +1,6 @@
 __version__ = '0.2.0'
 
 from .data_from_overpass import CaiOsmData
-from .data_from_overpass import CaiCounterHandler
 from .data_print import CaiOsmReport
 from .data_report import CaiOsmTable
 from .osmium_handler import CaiRoutesHandler
