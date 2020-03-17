770118842da6594005eb0420fb059974509ecd2e
Luca Delucchi <lucadeluge@gmail.com>
2020-03-17 10:03:53 +0100

    added class to manage difference and send changeset to mail and telegram

--- caiosm/data_diff.py
+++ caiosm/data_diff.py
@@ -5,18 +5,73 @@ Created on Sun May 19 22:18:37 2019
 
 @author: lucadelu
 """
-
+import os
+import configparser
 from caiosm.data_from_overpass import CaiOsmRouteDiff
-from caiosm.data_from_overpass import CaiOsmSourceRef
+from caiosm.functions import send_mail
+import telegram
 
 class ManageChanges():
-    
-    def __init__(self, type='sezioni', sendby='mail'):
-        self.type = type
-        self.sendby = sendby
-        if self.type == 'sezioni':
-            self.cosr = CaiOsmSourceRef()
-            self.cosr.get_list_codes()
-            
-    def send_mail():
-        """"""
+
+    def __init__(self, area=None, sourceref=None, startdate=None,
+                 enddate=None, daydiff=1, path=None):
+        """
+        params str area: area to query
+        params str sourceref: sezione code
+        """
+        if sourceref and area:
+            print("Please select only 'area' or 'sourceref'")
+        if sourceref:
+            self.cord = CaiOsmRouteDiff(sourceref=sourceref, enddate=enddate,
+                                        startdate=startdate)
+            self.title = "Aggiornamento dati per la sezione {}\n\n".format()
+        elif area:
+            self.cord = CaiOsmRouteDiff(area=area, startdate=startdate,
+                                        enddate=enddate)
+            self.title = "Aggiornamento dati per {}\n\n".format(area)
+        self.changes = self.cord.get_changeset()
+        if not self.changes:
+            print("No changes")
+            return False
+        self.text = self._set_text()
+        self.config = configparser.ConfigParser()
+        if path:
+            self.config.read(os.path.join(path, 'cai_scripts.ini'))
+        else:
+            self.config.read(os.path.join(os.path.expanduser('~'), 'cai_scripts.ini'))
+
+    def _set_text(self):
+        """Prepare text for message"""
+        if len(self.changes) == 1:
+            outext = "C'è stato 1"
+        elif len(self.changes) > 1:
+            outext = "Ci sono stati {nc}".format(nc=len(self.changes))
+        outext += " changeset tra {sta} e {end} con modifiche che ti " \
+                  "interessano:\n\n".format(sta=self.cord.startdate,
+                                            end=self.cord.enddate)
+        for c in self.changes:
+            outext += "* https://overpass-api.de/achavi/?changeset={idc}\n".format(idc=c)
+
+        return self.title + outext
+
+    def mail(self, bccs, path=None):
+        """Function to send mail with new changeset
+
+        :params list bccs: a list of mails to send in bbc
+        :params str path: the path to the ini file
+        """
+        send_mail(sub="Aggiornamento dai CAI in OSM", mess=self.text, bcc=bccs,
+                  path=path)
+
+    def telegram(self, token=None, chatid=None):
+        """Function to telegram update with new changeset
+
+        :params str token: the token of the bot to use
+        """
+        if not token:
+            token = self.config['TELEGRAM']['token']
+        if not chatid:
+            chatid = self.config['TELEGRAM']['chatid']
+        bot = telegram.Bot(token=token)
+        bot.send_message(chat_id=chatid, text=self.text)
+        return True
