49081cce3033c6a2032296298aeafc8d0cbfaf4c
Luca Delucchi <lucadeluge@gmail.com>
2020-03-17 17:16:31 +0100

    improved configuration

--- app/__init__.py
+++ app/__init__.py
@@ -60,15 +60,12 @@ def create_app(test_config=None):
     # create and configure the app
     app = Flask(__name__, instance_relative_config=True)
     app.config.from_mapping(
-        SECRET_KEY='dev',
-        DATABASE=os.path.join(app.instance_path, 'caiosm.sqlite'),
+        SQLALCHEMY_DATABASE_URI=config['DATABASE']['SQLALCHEMY_DATABASE_URI'],
     )
     cosr = CaiOsmSourceRef()
     cosr.get_list_codes()
-    if test_config is None:
-        # load the instance config, if it exists, when not testing
-        app.config.from_pyfile('config.py', silent=True)
-    else:
+
+    if test_config is not None:
         # load the test config if passed in
         app.config.from_mapping(test_config)
 
